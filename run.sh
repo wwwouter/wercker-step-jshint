1e0b568ff08e7afd46f32570d1418b2add6a0ebc
wwwouter <wouter@wercker.com>
2013-04-29 16:20:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 cd $WERCKER_ROOT
 if [ -e ".jshintignore" ] || [ -e ".jshintrc" ] ; then
-  echo ".jshintrc or .jshintignore found"
+	echo ".jshintrc or .jshintignore found"
 else
-  echo ".jshintrc or .jshintignore not found, stopping"
+	echo ".jshintrc or .jshintignore not found, skipping step"
 	exit 0
 fi
 
